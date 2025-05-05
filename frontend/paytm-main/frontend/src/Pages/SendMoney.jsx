52b14d6dbcf6b02ed08842d0dd8cffff5f2a5e0a
vivek <viveksr012@gmail.com>
2025-05-05 11:39:22 +0530

    implemented transfer route

--- frontend/paytm-main/frontend/src/Pages/SendMoney.jsx
+++ frontend/paytm-main/frontend/src/Pages/SendMoney.jsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const SendMoney = () => {
   const [searchParams] = useSearchParams();
@@ -9,58 +9,74 @@ export const SendMoney = () => {
   const [amount, setAmount] = useState(0);
 
   return (
-    <div class="flex justify-center h-screen bg-gray-100">
+    <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
-        <div class="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
-          <div class="flex flex-col space-y-1.5 p-6">
-            <h2 class="text-3xl font-bold text-center">Send Money</h2>
+        <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
+          <div className="flex flex-col space-y-1.5 p-6">
+            <h2 className="text-3xl font-bold text-center">Send Money</h2>
           </div>
-          <div class="p-6">
-            <div class="flex items-center space-x-4">
-              <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
+          <div className="p-6">
+            <div className="flex items-center space-x-4">
+              <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                <span className="text-2xl text-white">
+                  {name[0].toUpperCase()}
+                </span>
               </div>
-              <h3 class="text-2xl font-semibold">{name}</h3>
+              <h3 className="text-2xl font-semibold">{name}</h3>
             </div>
-            <div class="space-y-4">
-              <div class="space-y-2">
+            <div className="space-y-4">
+              <div className="space-y-2">
                 <label
-                  class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  for="amount"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor="amount"
                 >
                   Amount (in Rs)
                 </label>
+
                 <input
                   onChange={(e) => {
                     setAmount(e.target.value);
                   }}
+                  value={amount}
                   type="number"
-                  class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
+                  className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
                 />
               </div>
-              <div className="text-red-600 ">please enter valid detail</div>
-              <button
-                onClick={() => {
-                  axios.post(
-                    "http://localhost:3000/api/v1/account/transfer",
-                    {
-                      to: id,
-                      amount,
-                    },
-                    {
-                      headers: {
-                        Authorization:
-                          "Bearer " + localStorage.getItem("token"),
+              {amount >= 0 ? (
+                ""
+              ) : (
+                <div className="text-red-600 ">please enter valid detail</div>
+              )}
+
+              {amount > 0 ? (
+                <button
+                  onClick={() => {
+                    axios.post(
+                      "http://localhost:3000/api/v1/account/transfer",
+                      {
+                        to: id,
+                        amount,
                       },
-                    }
-                  );
-                }}
-                class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
-              >
-                Initiate Transfer
-              </button>
+                      {
+                        headers: {
+                          Authorization:
+                            "Bearer " + localStorage.getItem("token"),
+                        },
+                      }
+                    );
+                    setAmount(0);
+                  }}
+                  className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-400"
+                >
+                  Initiate Transfer
+                </button>
+              ) : (
+                <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-gray-600 text-white hover:bg-gray-500">
+                  Send Money
+                </button>
+              )}
             </div>
           </div>
         </div>
